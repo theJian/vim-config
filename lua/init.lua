245b9356cf303bbaf34eebb478b104041b6d72a4
theJian <thejianmail@gmail.com>
2020-04-25 17:04:09 +0800

    Update on 04-25-20 17:04

--- lua/init.lua
+++ lua/init.lua
@@ -1,8 +1,11 @@
 local api = vim.api
+local fn = vim.fn
 
 if api.nvim_get_option('loadplugins') then
 	require 'packman'
 	local lsp = require 'nvim_lsp'
 	lsp.tsserver.setup{}
-	lsp.gopls.setup{}
+	lsp.gopls.setup{
+		cmd = { fn.trim(fn.system('go env GOPATH')) .. "/bin/gopls" };
+	}
 end
