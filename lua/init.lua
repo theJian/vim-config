9e292bd41a559d98dcfcae6848a39659ce389766
theJian <thejianmail@gmail.com>
2020-04-28 21:34:32 +0800

    Update on 04-28-20 21:34

--- lua/init.lua
+++ lua/init.lua
@@ -4,6 +4,10 @@ local fn = vim.fn
 if api.nvim_get_option('loadplugins') then
 	require 'packman'
 	local lsp = require 'nvim_lsp'
+	lsp.vimls.setup{}
+	lsp.rust_analyzer.setup{}
+	lsp.pyls_ms.setup{}
+	lsp.bashls.setup{}
 	lsp.tsserver.setup{}
 	lsp.jsonls.setup{}
 	lsp.cssls.setup{}
