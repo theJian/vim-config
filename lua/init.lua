6ac8f33afc753ef22fc67d184af33f9da84e7d16
theJian <thejianmail@gmail.com>
2020-05-13 18:06:57 +0800

    Update on 05-13-20 18:06

--- lua/init.lua
+++ lua/init.lua
@@ -3,10 +3,13 @@ local fn = vim.fn
 
 if api.nvim_get_option('loadplugins') then
 	require 'packman'
+	local homedir = vim.loop.os_homedir()
 	local lsp = require 'nvim_lsp'
 	lsp.vimls.setup{}
 	lsp.rust_analyzer.setup{}
-	lsp.pyls_ms.setup{}
+	lsp.pyls_ms.setup{
+		cmd = { "dotnet", "exec", homedir .. "/bin/languageServer/pyls_ms/Microsoft.Python.LanguageServer.dll" };
+	}
 	lsp.bashls.setup{}
 	lsp.tsserver.setup{}
 	lsp.jsonls.setup{}
