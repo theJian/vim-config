ce18c2150b9cddea2278bc2a8ef7972cc0f352a7
Jiàn <thejianmail@gmail.com>
2023-12-19 18:27:10 +0800

    backup 12-19-23 18:27

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -365,7 +365,7 @@ require'gitsigns'.setup{
 	signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
 	numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
 	linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
-	word_diff  = true, -- Toggle with `:Gitsigns toggle_word_diff`
+	word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
 	watch_gitdir = {
 		follow_files = true
 	},
