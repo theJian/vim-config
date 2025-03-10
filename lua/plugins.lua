421fb1c32cb335e9c5919cf34c11efbe8d83ed41
Jiàn <hi@chabuduo.in>
2025-03-10 13:18:35 +0800

    backup 03-10-25 13:18

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -182,7 +182,7 @@ api.nvim_create_autocmd('LspAttach', {
 	callback = function (ev)
 		local opts = { buffer = ev.buf }
 		local client = vim.lsp.get_client_by_id(ev.data.client_id)
-		-- vim.keymap.set('n', '<c-w>d', vim.diagnostic.open_float, opts)
+		vim.keymap.set('n', 'gK', vim.diagnostic.open_float, opts)
 
 		vim.keymap.set('n', 'g0', vim.diagnostic.setloclist)
 
@@ -190,8 +190,8 @@ api.nvim_create_autocmd('LspAttach', {
 		-- vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 
 		vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, opts)
-      vim.keymap.set('n', 'gd', vim.lsp.buf.declaration, opts)
-      vim.keymap.set('n', '<c-]>', vim.lsp.buf.definition, opts)
+      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
       -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
       vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, opts)
       vim.keymap.set('n', '<leader>wn', vim.lsp.buf.add_workspace_folder, opts)
@@ -201,8 +201,8 @@ api.nvim_create_autocmd('LspAttach', {
       end, opts)
       vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, opts)
       vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-      vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+      vim.keymap.set({ 'n', 'v' }, 'g.', vim.lsp.buf.code_action, opts)
+      vim.keymap.set('n', 'gR', vim.lsp.buf.references, opts)
       vim.keymap.set('n', 'gO', vim.lsp.buf.document_symbol, opts)
       vim.keymap.set('n', '<leader>fm', function()
         vim.lsp.buf.format { async = true }
