12c30eaf678eb23519c809ba94acd0927623ae85
Jiàn <hi@chabuduo.in>
2025-12-27 23:49:15 +0800

    backup 12-27-25 23:49

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -75,6 +75,11 @@ api.nvim_create_autocmd({ 'InsertLeave', 'TextChanged' }, {
 -- restore cursor to file position in previous editing session
 api.nvim_create_autocmd('BufReadPost', {
 	callback = function(args)
+		-- ignore terminal buffers
+		if vim.bo[args.buf].buftype == 'terminal' then
+			return
+		end
+
 		local mark = api.nvim_buf_get_mark(args.buf, '"')
 		local line_count = api.nvim_buf_line_count(args.buf)
 		if mark[1] > 0 and mark[1] <= line_count then
