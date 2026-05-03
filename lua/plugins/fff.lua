c2e5568fdbba24a3fb4f0ea5178d95ec341b7d5b
Jiàn <hi@chabuduo.in>
2026-05-03 23:57:18 +0800

    refactor: replace fit plugin with fff.nvim
    Switch from custom fit/fzy file finder to dmtrKovalenko/fff.nvim
    using Neovim's built-in vim.pack.add.

---
+++ lua/plugins/fff.lua
@@ -0,0 +1,22 @@
+vim.pack.add { 'https://github.com/dmtrKovalenko/fff.nvim' }
+
+vim.api.nvim_create_autocmd('PackChanged', {
+	callback = function(ev)
+		local name, kind = ev.data.spec.name, ev.data.kind
+		if name == 'fff.nvim' and (kind == 'install' or kind == 'update') then
+			if not ev.data.active then
+				vim.cmd.packadd 'fff.nvim'
+			end
+			require('fff.download').download_or_build_binary()
+		end
+	end,
+})
+
+vim.g.fff = {
+	lazy_sync = true,
+	debug = { enabled = true, show_scores = true },
+}
+
+vim.keymap.set('n', 'ff', function()
+	require('fff').find_files()
+end, { desc = 'FFFind files' })
